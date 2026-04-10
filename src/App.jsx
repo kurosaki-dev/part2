b247fc0745a047252142a8cbffe10b5472cf4136
Kael <269341204+kurosaki-dev@users.noreply.github.com>
2026-04-10 14:35:59 +0800

    2.8: The Phonebook Step 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { id: 1, name: "Arto Hellas", number: "046-12-345" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
+    if (newName.trim() === "" || newNumber.trim() === "") {
+      alert("Please fill in both name and number.");
+      return;
+    }
+
     const personObject = {
+      id: String(persons.length + 1),
       name: newName,
+      number: newNumber,
     };
 
     // first solution (the one that i came up with)
@@ -29,17 +39,35 @@ const App = () => {
       return persons.some((person) => person.name === name);
     };
 
+    const isNumberExist = (number) => {
+      return persons.some((person) => person.number === number);
+    };
+
+    const emptyInputStates = () => {
+      setNewName("");
+      setNewNumber("");
+    };
+
     if (isPersonExist(personObject.name)) {
       alert(`${personObject.name} is already added to phonebook`);
-      setNewName("");
+      emptyInputStates();
+    } else if (isNumberExist(personObject.number)) {
+      alert(`${personObject.number} is already registered.`);
+      emptyInputStates();
     } else {
       setPersons(persons.concat(personObject));
-      setNewName("");
+      emptyInputStates();
     }
   };
 
+  // name input
   const handlePersonChange = (event) => {
     setNewName(event.target.value);
+  };
+
+  // number input
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
   };
 
   return (
@@ -50,12 +78,17 @@ const App = () => {
           name: <input value={newName} onChange={handlePersonChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map((person) => (
-        <p key={person.name}>{person.name}</p>
+        <p key={person.id}>
+          {person.name} <span>{person.number}</span>
+        </p>
       ))}
     </div>
   );
